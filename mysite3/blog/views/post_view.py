4605dc0468e217929d191d1c5be84c72671f32f6
Antonio Annes <antonio.vmachado13@gmail.com>
2023-07-24 21:34:00 -0300

    html

--- mysite3/blog/views/post_view.py
+++ mysite3/blog/views/post_view.py
@@ -1,9 +1,13 @@
-from django.http import HttpResponse
 from django.views import generic
 
+from mysite3.blog.models import Post
 
 
 class PostView(generic.View):
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    template_name = 'index.html'
 
-    def get(self, request, *args, **kwargs):
-        return HttpResponse('Helloo World')
+    
+ class PostDetail(generic.DetailView):
+     model = Post
+     template_name = 'post_detail.html'
